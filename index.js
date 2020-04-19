b7bc5f8580c1d128ec08d037be82b7593884b7e8
Kim Maida <yi.mihi@gmail.com>
2020-04-19 12:37:32 -0400

    Removed bot commands section

--- index.js
+++ index.js
@@ -169,49 +169,6 @@ app.event('app_mention', async({ event, context }) => {
 });
 
 /*------------------
-    BOT COMMANDS
-------------------*/
-
-app.command('/twirota-assign', async({ command, ack, say }) => {
-  await ack();
-  console.log(command);
-  const assignment = `<${command.text}>`;
-  // try {
-    const list = JSON.parse(fs.readFileSync(rotaFile));
-    list['twirota'] = assignment;
-    fs.writeFileSync(rotaFile, JSON.stringify(list, null, 2));
-    console.log(list);
-  // } catch (e) {
-  //   await say('An error has occurred while trying to assign the concierge.\n```' + JSON.stringify(e) + '```');
-  // }
-  await say(`${assignment} is now on-call for rotation concierge duties.`);
-});
-
-app.command('/twirota-who', async ({ command, ack, say }) => {
-  await ack();
-  const list = JSON.parse(fs.readFileSync(rotaFile));
-  const oncallName = list['twirota'];
-  if (oncallName) {
-    await say('The Twitter rotation concierge is `' + oncallName + '`. To notify them directly, mention `@twirota` in your message.');
-  } else {
-    await say('Nobody is currently assigned for Twitter rotation. To assign someone, use `/twirota-assign [@user]`');
-  }
-});
-
-app.command('/twirota-clear', async ({ command, ack, say }) => {
-  await ack();
-  const list = JSON.parse(fs.readFileSync(rotaFile));
-
-  if (list['twirota']) {
-    delete list['twirota'];
-    fs.writeFileSync(rotaFile, JSON.stringify(list, null, 2));
-    await say('Twitter rotation concierge has been unassigned.');
-  } else {
-    await say('There is currently nobody assigned for Twitter rotation.');
-  }
-});
-
-/*------------------
      START APP
 ------------------*/
 (async () => {
