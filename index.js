291b71d78a6dbd4b7faf3621f3f59c4f814641ad
Kim Maida <yi.mihi@gmail.com>
2020-04-19 10:27:52 -0400

    Slash command for assignment is working

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 // Require the Bolt package (github.com/slackapi/bolt)
 const { App } = require('@slack/bolt');
+const rota = require('./rota');
+const fs = require('fs');
+const rotaFile = './rotations.json';
+
+console.log(rotaFile);
+
 // Create Bolt app
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -8,23 +14,83 @@ const app = new App({
 });
 const port = process.env.PORT || 3000;
 
-/* BOT FUNCTIONALITY */
+/*------------------
+     UTILITIES
+------------------*/
+
+const jsonReader = (filePath, cb) => {
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      return cb && cb(err);
+    }
+    try {
+      const obj = JSON.parse(fileData);
+      return cb && cb(null, obj);
+    }
+    catch (err) {
+      return cb && cb(err);
+    }
+  });
+}
+
+jsonReader(rotaFile, (err, twirota) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(twirota);
+});
+
+/*------------------
+    BOT EVENTS
+------------------*/
+
 app.event('app_mention', async({ event, context }) => {
-  console.log(event, context);
   try {
     const result = await app.client.chat.postMessage({
       token: context.botToken,
       channel: event.channel,
       text: 'You mentioned me!'
     });
-    console.log(result);
   }
   catch (error) {
     console.error(error);
   }
+  // Bot commands
+  console.log('Event: ', event, 'Context: ', context);
 });
 
-// Start Bolt app
+/*------------------
+    BOT COMMANDS
+------------------*/
+
+app.command('/twirota', async({ command, ack, say }) => {
+  await ack();
+  const commandText = command.text;
+  const textCaseNormalized = commandText.toLowerCase();
+  console.log(command);
+
+  if (textCaseNormalized.includes('assign @')) {
+    const assignment = `<${textCaseNormalized.split('assign ')[1]}>`;
+    // try {
+      let list = fs.readFileSync(rotaFile);
+      list = JSON.parse(list);
+      list['twirota'] = assignment;
+      fs.writeFileSync(rotaFile, JSON.stringify(list, null, 2));
+      console.log(list);
+    // } catch (e) {
+    //   return 'An error has occurred while trying to assign the concierge.\n```' + JSON.stringify(e) + '```';
+    // }
+    await say(`${assignment} is now on-call for rotation concierge duties.`);
+  }
+  if (textCaseNormalized === 'who' || textCaseNormalized === 'on-call') {
+    await say(`Checking that for you...`);
+  }
+});
+
+/*------------------
+     START APP
+------------------*/
 (async () => {
   await app.start(port);
   console.log(`⚡️ Rota is running on ${port}!`);
--- rota.js
+++ rota.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const dirname = require('path').dirname;
 const rotaFile = dirname(__dirname) + '/rotations.json';
 
-const rota = (context) => {
+const rota = (event, context) => {
   // Initialize concierge on first run
   try {
     fs.accessSync(rotaFile, fs.F_OK);
@@ -16,39 +16,39 @@ const rota = (context) => {
   }
 
   return {
-    callTwirota: function (req, res) {
+    callTwirota: (event) => {
       try {
         const list = fs.readFileSync(rotaFile);
         list = JSON.parse(list);
 
         if (!list['twirota']) {
-          return res.text('No Twitter Rotation assigned. Use `@twirota assign {@username}`').send();
+          return 'No Twitter Rotation assigned. Use `@twirota assign {@username}`';
         }
 
         const link = `https://${process.env.SLACK_TEAM}.slack.com/archives/${req.channel.name}/p${req.message.timestamp.replace('.', '')}`;
         const conciergeMessage = `@${req.from.name} needs your attention in #${req.channel.name} (${link}) \n\n*Message*:\n`;
-        res.text(conciergeMessage + req.message.value.text, list['twirota']);
+        // TODO: send a DM to the concierge: conciergeMessage + req.message.value.text, list['twirota'];
 
-        return res.text('A message has been sent to the Twitter Rotation concierge. If your message is urgent and you don\'t receive a reply, please use `@here` or `@channel`.').send();
+        return 'A message has been sent to the Twitter Rotation concierge. If your message is urgent and you don\'t receive a reply, please use `@here` or `@channel`.';
       } catch (e) {
-        return res.text('An error has occurred while trying to contact the concierge.\n```' + JSON.stringify(e) + '```').send();
+        return 'An error has occurred while trying to contact the concierge.\n```' + JSON.stringify(e) + '```';
       }
     },
-    whosTwirota: function (req, res) {
+    whosTwirota: (event) => {
       try {
         const list = fs.readFileSync(rotaFile);
         list = JSON.parse(list);
 
         const conciergeName = list['twirota'];
         if (conciergeName) {
-          return res.text('The Twitter Rotation concierge is `' + conciergeName + '`. To send a direct message to the Twitter Rotation concierge use `@twirota message: {text}`').send();
+          return 'The Twitter Rotation concierge is `' + conciergeName + '`. To send a direct message to the Twitter Rotation concierge use `@twirota message: {text}`';
         }
-        return res.text('No Twitter Rotation concierge is currently assigned.').send();
+        return 'No Twitter Rotation concierge is currently assigned.';
       } catch (e) {
-        return res.text('An error has occurred while trying to assign the concierge.\n```' + JSON.stringify(e) + '```').send();
+        return 'An error has occurred while trying to assign the concierge.\n```' + JSON.stringify(e) + '```';
       }
     },
-    assignTwirota: function (req, res) {
+    assignTwirota: (event) => {
       try {
         const list = fs.readFileSync(rotaFile);
         list = JSON.parse(list);
@@ -60,12 +60,12 @@ const rota = (context) => {
 
         list['twirota'] = name;
         fs.writeFileSync(rotaFile, JSON.stringify(list, null, 2));
-        return res.text('User ' + name + ' has been assigned Twitter Rotation concierge.').send();
+        return 'User ' + name + ' has been assigned Twitter Rotation concierge.';
       } catch (e) {
-        return res.text('An error has occurred while trying to assign the concierge.\n```' + JSON.stringify(e) + '```').send();
+        return 'An error has occurred while trying to assign the concierge.\n```' + JSON.stringify(e) + '```';
       }
     },
-    clearTwirota: function (req, res) {
+    clearTwirota: (event) => {
       try {
         var list = fs.readFileSync(rotaFile);
         list = JSON.parse(list);
@@ -73,17 +73,17 @@ const rota = (context) => {
         if (list['twirota']) {
           delete list['twirota'];
           fs.writeFileSync(rotaFile, JSON.stringify(list, null, 2));
-          return res.text('Unassigned concierge for Twitter Rotation.').send();
+          return 'Unassigned concierge for Twitter Rotation.';
         }
 
-        return res.text('There is currently no one assigned to Twitter Rotation. Nothing changed.').send();
+        return 'There is currently no one assigned to Twitter Rotation. Nothing changed.';
       } catch (e) {
-        return res.text('An error has occurred while trying to unassign the Twitter Rotation.\n```' + JSON.stringify(e) + '```').send();
+        return 'An error has occurred while trying to unassign the Twitter Rotation.\n```' + JSON.stringify(e) + '```';
       }
     },
-    help: function (req, res) {
-      var helpMessage = "Here\'s what I can do:\n- Use `@twirota who` to check who is currently assigned for Twitter Rotation.\n- Use `@twirota assign {@username}` to assign a person to Twitter Rotation.\n- Use `@twirota message {text}` to send a direct message to the concierge on rotation.\n- Use `@twirota stop on-call` to clear the assignment.";
-      return res.text(helpMessage).send();
+    help: (event) => {
+      const helpMessage = "Here\'s what I can do:\n- Use `@twirota who` to check who is currently assigned for Twitter Rotation.\n- Use `@twirota assign {@username}` to assign a person to Twitter Rotation.\n- Use `@twirota message {text}` to send a direct message to the concierge on rotation.\n- Use `@twirota stop on-call` to clear the assignment.";
+      return helpMessage;
     }
   };
 };
